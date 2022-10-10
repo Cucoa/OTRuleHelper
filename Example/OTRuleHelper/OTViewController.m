d76dc162d4ad6fc95f75ec056cd360f11a97304b
Any <2460651872@qq.com>
2022-10-10 15:40:02 +0800

    新增示例1

--- Example/OTRuleHelper/OTViewController.m
+++ Example/OTRuleHelper/OTViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "OTViewController.h"
+#import <OTRuleHelper/OTRuleHelper.h>
 
 @interface OTViewController ()
 
@@ -18,12 +19,29 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self sample1];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)sample1{
+    NSArray *rules = @[
+        @{
+            OTRuleKeyItemName : @"uid",
+            OTRuleKeyItemSize : @(4),
+            OTRuleKeyItemType : @(OTRuleValueTypeInt32),
+        },
+        @{
+            OTRuleKeyItemName : @"token",
+            OTRuleKeyItemSize : @(16),
+            OTRuleKeyItemType : @(OTRuleValueTypeStringUTF8),
+        },
+    ];
+    NSDictionary *dict = @{
+        @"uid":@(1025),
+        @"token":@"A8C864D4C95464E8",
+    };
+    NSData *data = [OTRuleHelper dataForDictionary:dict withRules:rules];
+    NSDictionary *json = [OTRuleHelper dictionaryForData:data withRules:rules];
+    NSLog(@"~~>Data:%@",data);
+    NSLog(@"~~>Json:%@",json);
 }
-
 @end
